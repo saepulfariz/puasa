4539a965b08fcd1c9c07d5efc777c787ba6fa954
saepulfariz <saepulfariz108@gmail.com>
2021-04-09 06:08:42 +0700

    Tambah File Lainnya

---
+++ index.php
@@ -0,0 +1,476 @@
+<?php 
+
+// script by saepulfariz 08/04/2021 versi PHP
+
+$puasa = [
+	[
+		"tanggal" => 1,
+		"imsak" => "04:25",
+		"subuh" => "04:35",
+		"terbit" => "05:43",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:53",
+		"ashar" => "15:11",
+		"magrib" => "17:53",
+		"isya" => "19:00"
+	],
+	[
+		"tanggal" => 2,
+		"imsak" => "04:25",
+		"subuh" => "04:35",
+		"terbit" => "05:43",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:52",
+		"ashar" => "15:11",
+		"magrib" => "17:53",
+		"isya" => "19:00"
+	],
+	[
+		"tanggal" => 3,
+		"imsak" => "04:24",
+		"subuh" => "04:34",
+		"terbit" => "05:43",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:52",
+		"ashar" => "15:11",
+		"magrib" => "17:52",
+		"isya" => "19:00"
+	],
+	[
+		"tanggal" => 4,
+		"imsak" => "04:24",
+		"subuh" => "04:34",
+		"terbit" => "05:43",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:52",
+		"ashar" => "15:10",
+		"magrib" => "17:52",
+		"isya" => "18:59"
+	],
+	[
+		"tanggal" => 5,
+		"imsak" => "04:24",
+		"subuh" => "04:34",
+		"terbit" => "05:43",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:52",
+		"ashar" => "15:10",
+		"magrib" => "17:52",
+		"isya" => "18:59"
+	],
+	[
+		"tanggal" => 6,
+		"imsak" => "04:24",
+		"subuh" => "04:34",
+		"terbit" => "05:43",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:51",
+		"ashar" => "15:10",
+		"magrib" => "17:51",
+		"isya" => "18:59"
+	],
+	[
+		"tanggal" => 7,
+		"imsak" => "04:24",
+		"subuh" => "04:34",
+		"terbit" => "05:43",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:51",
+		"ashar" => "15:10",
+		"magrib" => "17:51",
+		"isya" => "18:58"
+	],
+	[
+		"tanggal" => 8,
+		"imsak" => "04:24",
+		"subuh" => "04:34",
+		"terbit" => "05:43",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:51",
+		"ashar" => "15:10",
+		"magrib" => "17:50",
+		"isya" => "18:58"
+	],
+	[
+		"tanggal" => 9,
+		"imsak" => "04:24",
+		"subuh" => "04:34",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:51",
+		"ashar" => "15:10",
+		"magrib" => "17:50",
+		"isya" => "18:58"
+	],
+	[
+		"tanggal" => 10,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:51",
+		"ashar" => "15:10",
+		"magrib" => "17:50",
+		"isya" => "18:57"
+	],
+	[
+		"tanggal" => 11,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:50",
+		"ashar" => "15:10",
+		"magrib" => "17:49",
+		"isya" => "18:57"
+	],
+	[
+		"tanggal" => 12,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:50",
+		"ashar" => "15:10",
+		"magrib" => "17:49",
+		"isya" => "18:57"
+	],
+	[
+		"tanggal" => 13,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:50",
+		"ashar" => "15:10",
+		"magrib" => "17:49",
+		"isya" => "18:57"
+	],
+	[
+		"tanggal" => 14,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:50",
+		"ashar" => "15:10",
+		"magrib" => "17:48",
+		"isya" => "18:56"
+	],
+	[
+		"tanggal" => 15,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:50",
+		"ashar" => "15:10",
+		"magrib" => "17:48",
+		"isya" => "18:56"
+	],
+	[
+		"tanggal" => 16,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:50",
+		"ashar" => "15:10",
+		"magrib" => "17:48",
+		"isya" => "18:56"
+	],
+	[
+		"tanggal" => 17,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:49",
+		"ashar" => "15:10",
+		"magrib" => "17:47",
+		"isya" => "18:56"
+	],
+	[
+		"tanggal" => 18,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:14",
+		"dzuhur" => "11:49",
+		"ashar" => "15:10",
+		"magrib" => "17:47",
+		"isya" => "18:56"
+	],
+	[
+		"tanggal" => 19,
+		"imsak" => "04:23",
+		"subuh" => "04:33",
+		"terbit" => "05:42",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:47",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 20,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:42",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:47",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 20,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:42",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:46",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 21,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:42",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:46",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 22,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:42",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:46",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 23,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:43",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:46",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 24,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:43",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:46",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 25,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:43",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:46",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 26,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:43",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:45",
+		"isya" => "18:55"
+	],
+	[
+		"tanggal" => 27,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:43",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:49",
+		"ashar" => "15:09",
+		"magrib" => "17:45",
+		"isya" => "18:54"
+	],
+	[
+		"tanggal" => 28,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:43",
+		"dhuha" => "06:15",
+		"dzuhur" => "11:48",
+		"ashar" => "15:09",
+		"magrib" => "17:45",
+		"isya" => "18:54"
+	],
+	[
+		"tanggal" => 29,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:43",
+		"dhuha" => "06:16",
+		"dzuhur" => "11:48",
+		"ashar" => "15:09",
+		"magrib" => "17:45",
+		"isya" => "18:54"
+	],
+	[
+		"tanggal" => 30,
+		"imsak" => "04:22",
+		"subuh" => "04:32",
+		"terbit" => "05:43",
+		"dhuha" => "06:16",
+		"dzuhur" => "11:48",
+		"ashar" => "15:09",
+		"magrib" => "17:45",
+		"isya" => "18:54"
+	],
+];
+
+
+// ini ketika kita ambil data puasanya lewat file json atau bisa dari api juga.
+$puasa = file_get_contents('puasa.json');
+
+$puasa = json_decode($puasa, true);
+
+
+
+// echo json_encode($puasa); die();
+
+
+ ?>
+
+
+ <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Jadwal Imsakiyah | PROFA (Programmer Fasilkom)</title>
+
+    <style>
+    	@font-face{
+			font-family:"IndieFlower";
+			src:url("fonts/IndieFlower-Regular.ttf");
+			font-weight:normal;
+		}
+
+		/*https://fonts.google.com/specimen/Indie+Flower?preview.text=Jadwal%20Imsakiyah&preview.text_type=custom*/
+
+    	body{
+    		background-image: url('images/bg.jpg');
+    		background-size: cover;
+			background-attachment: fixed;
+			background-position: center;
+    	}
+
+    	h1{
+    		font-family: IndieFlower;
+    	}
+    	
+    	tr:nth-child(even) {
+    		/*background: #CCC;*/
+    		color: yellow;
+    	}
+		tr:nth-child(odd) {
+			/*background: #FFF;*/
+			/*color: red;*/
+		}
+
+		tr:hover{
+			background-color: #00FFFFFF;
+		}
+
+
+		.table-responsive{
+			height: 500px;
+			overflow: auto;
+		}
+    </style>
+  </head>
+  <body>
+    <div class="container">
+    	<div class="row mt-4 justify-content-center">
+    		<div class="col-md-10 text-center bg-info">
+    			<h1 class="text-danger pb-2 pt-4">Jadwal Imsakiyah | PROFA (Programmer Fasilkom)</h1>
+    		</div>
+    	</div>
+    	<div class="row justify-content-center mt-4 mb-3">
+    		<div class="col-md-8">
+    			<div class="card bg-primary shadow-sm">
+    				<div class="card-body">
+    					<div class="table-responsive">
+    						<!-- table-striped -->
+    						<table class="table text-white table-hover ">
+							  <thead>
+							    <tr>
+							      <th scope="col">Tanggal</th>
+							      <th scope="col">IMSAK</th>
+							      <th scope="col">SUBUH</th>
+							      <th scope="col">TERBIT</th>
+							      <th scope="col">DHUHA</th>
+							      <th scope="col">DZUHUR</th>
+							      <th scope="col">ASHAR</th>
+							      <th scope="col">MAGRIB</th>
+							      <th scope="col">ISYA</th>
+							    </tr>
+							  </thead>
+							  <tbody>
+							    <?php foreach ($puasa as $key): ?>
+							    	<tr>
+							    		<td><?= $key['tanggal']; ?> Ramadan</td>
+							    		<td class="bg-success"><?= $key['imsak']; ?></td>
+							    		<td><?= $key['subuh']; ?></td>
+							    		<td><?= $key['terbit']; ?></td>
+							    		<td><?= $key['dhuha']; ?></td>
+							    		<td><?= $key['dzuhur']; ?></td>
+							    		<td><?= $key['ashar']; ?></td>
+							    		<td class="bg-success"><?= $key['magrib']; ?></td>
+							    		<td><?= $key['isya']; ?></td>
+							    	</tr>
+							    <?php endforeach ?>
+							  </tbody>
+							</table>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
